c39c2afa05244c4aa724d97399286fbc93982501
Alex Kh. <sanchez.primal@gmail.com>
2026-03-27 11:57:41 +0100

    fix adhesion to checklist

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ export const App = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                onChange={e => setQuery(e.target.value)}
+                onChange={event => setQuery(event.target.value)}
                 value={query}
               />
             </div>
